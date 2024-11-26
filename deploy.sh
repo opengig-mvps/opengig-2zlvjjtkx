32189fd31bc7e9abd76b6ec7be04a3930f4d3ea9
Nilesh darji <97864081+Nilesh9106@users.noreply.github.com>
2024-11-26 15:47:55 +0530

    DEPLOY

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-
+DOTENV_ME=$1
 REPO=$(hostname)
 cd "$HOME/$REPO"
 git pull
+
+# Install and authenticate with dotenv-vault
+npm install -g dotenv-vault@latest
+dotenv-vault login "${DOTENV_ME}"
+
+dotenv-vault pull ci
+
+cp .env.ci .env
+
+# Append dynamic variables
+echo "" >> .env
+echo "# Dynamic Variables" >> .env
+echo "DATABASE_URL=postgresql://opengig:Database2024!@opengigmvp.postgres.database.azure.com:5432/${REPO}?schema=public" >> .env
+echo "POSTGRES_DB=${REPO}" >> .env
+echo "NEXTAUTH_URL=https://${REPO}.opengig.work" >> .env
+echo "APP_URL=https://${REPO}.opengig.work" >> .env
+
+# Continue with regular deployment
 npm install
 npm run build
 
